ed94d4cbf0111dd2152f050dae7d101ef5e0c4e2
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-02-23 14:59:25 -0800

    [RBDI-5] Added view controller classes, added their views to the storyboard, and updated the ModelController to display the correct one.

---
+++ BigDataDemo/ConfigurationViewController.m
@@ -0,0 +1,23 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Copyright (c) 2016 Jaguar Land Rover.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Mozilla Public License, version 2.0. The full text of the
+ * Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
+ *
+ * File:    ConfigurationViewController.m
+ * Project: BigDataDemo
+ *
+ * Created by Lilli Szafranski on 2/23/16.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#import "ConfigurationViewController.h"
+
+
+@implementation ConfigurationViewController
+{
+
+}
+@end
