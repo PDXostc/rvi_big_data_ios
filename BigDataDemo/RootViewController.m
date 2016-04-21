949499d2d7ca2469db253647faac34f9bfce9821
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-04-21 15:28:12 -0700

    [RBDI-18] Added websocket start/stopping (and all singleton management) to the app delegate to better handle backgrounding app.

--- BigDataDemo/RootViewController.m
+++ BigDataDemo/RootViewController.m
@@ -25,10 +25,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    [SignalManager start];
-    [VehicleManager start];
-    [BackendServerManager start];
+//
+//    [SignalManager start];
+//    [VehicleManager start];
+//    [BackendServerManager start];
 
     // Do any additional setup after loading the view, typically from a nib.
     // Configure the page view controller and add it as a child view controller.
